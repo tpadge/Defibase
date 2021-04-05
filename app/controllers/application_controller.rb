c366cbdb1259c0ee0cda6023185d791f9c3e74e9
tpadgett <tim_padgett@yahoo.com>
2021-04-05 15:25:49 -0700

    initiate FE cycle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,8 +24,8 @@ class ApplicationController < ActionController::Base
 
   def require_logged_in
     unless current_user
-      render json: { base: ['Invalid credentials'] }, status 401
+      render json: { base: ['Invalid credentials'] }, status: 401
     end
   end
-  
+
 end
