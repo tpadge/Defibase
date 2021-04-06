b8f3f14993ea1282332f58fa9b113feeed815380
tpadgett <tim_padgett@yahoo.com>
2021-04-05 17:11:11 -0700

    complete redux auth

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { Provider } from 'react-redux';
+import React from "react";
+import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
+import App from "./app";
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <div>Hello, world!</div>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </Provider>
 );
 
--- frontend/defibase.jsx
+++ frontend/defibase.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
+import configureStore from './store/store'
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  // const store = configureStore();
+  const store = configureStore();
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome!</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
 });
