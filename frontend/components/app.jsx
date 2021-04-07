71b84ea869bfd8057892c431af57a247586f2c80
tpadgett <tim_padgett@yahoo.com>
2021-04-06 18:09:42 -0700

    splash styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,21 +2,24 @@ import React from "react";
 import NavbarContainer from './navbar/navbar_container';
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/signup_form_container';
-import {Route, Switch} from 'react-router-dom';
+import {Link, Route, Switch} from 'react-router-dom';
 import { AuthRoute } from '../utils/route_util';
 import Splash from './splash/splash';
+
 
 const App = (props) => (
   <div>
     <header>
-  <h1>defibase</h1>
-  <NavbarContainer/>
+      <Link to="/" className="header-link">
+        <h3>Defibase</h3>
+      </Link>
+    <NavbarContainer/>
     </header>
 
   <Switch>
     <AuthRoute path="/login" component={LoginFormContainer} />
     <AuthRoute path="/signup" component={SignupFormContainer} />
-    
+    <Route exact path="/" component={Splash} />
   </Switch>
   </div>
 
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import Navbar from '../navbar/navbar'
+import Navbar from '../navbar/navbar';
+import {Link} from 'react-router-dom';
+
 const Splash = (props) => {
   return (
     <main>
-      <Navbar />
-      <h1>defibase</h1>
-
+      <div className="splash-greet">
+      <h1>Buy and sell cryptocurrrency</h1>
+      <h2>Defibase is the easiest place to buy and sell cryptocurrrency.
+      Sign up to own part of the future of finance.</h2>
+        <button><Link to="/signup">Get Started</Link></button>
+      </div>
 
      
     </main>
