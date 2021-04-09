a9756a5d8fb6d4f63663a4bcf458878b5a4284fe
tpadgett <tim_padgett@yahoo.com>
2021-04-08 21:52:50 -0700

    complete index and index styling

--- frontend/components/coins/coin_index.jsx
+++ frontend/components/coins/coin_index.jsx
@@ -25,30 +25,61 @@ class CoinIndex extends React.Component {
   }
 
   render(){
+
     if (Object.keys(this.props.list).length === 0) return null ;
     const list = this.props.list
     return (
+      <div>
+        <section className="index-welcome">
+        <div classname="index-copy">
+        <h1>The future of finance is <span className="blue">decentralized.</span></h1>
+        <h3>View coins to learn more about defi</h3>
+        <h3>If you like a coin, add it to your watchlist</h3>
+                <div className="index-buttons">
+          <button className="watchlist">View your watchlist</button>
+        </div>
+        </div>
+        </section>
+
+      <div className="labels">
+        <div className="name-label">Name</div>
+        <div className="price">Price</div>
+        <div className="change">Change</div>
+        <div className="volume">Volume (24h)</div>
+        <div className="market-cap">Market cap</div>
+      </div>
+
       <div className="index-main">
         <ul>
           {Object.keys(this.props.list).map(title => {
             return (  
+              <div>
+
+              <section className="coins">
               <li className="index-list">
+              <div className="index-item">
                 <div className="basic-info">
                   <div><img className="badge" src={list[title].image}/></div>
-                  <div>{list[title].name}</div> 
-                  <div>{list[title].symbol.toUpperCase()}</div>
+                  <div className="full-name">{list[title].name}</div> 
+                  <div className="short-name">{list[title].symbol.toUpperCase()}</div>
                 </div>
                 <div className="price-info">
-                  <div className="price">${this.formatNumber(list[title].current_price)}</div>
+                  <div className="price">${this.formatNumber(list[title].current_price.toFixed(2))}</div>
                   <div className="change">{list[title].price_change_percentage_24h.toFixed(2)}%</div>
                   <div className="volume">${this.formatNumber(list[title].total_volume)}</div>
                   <div className="market-cap">${this.formatNumber(list[title].market_cap)}</div>
+                  <button className="view-button"><Link to={`/coins/${list[title].id}`}>View</Link></button>
+                  <button className="watch-button">Watch</button>
+                </div>
                 </div>
               </li>
-              // <p>{list[title].id}</p>
+              </section>
+              </div>
+              
             )
           })}
         </ul>
+      </div>
       </div>
       // <div className="index-main">
       //   <ul>
